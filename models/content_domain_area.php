5aa253503965e0378f8184da773ec00640b3e26b
Remo Laubacher <remo.laubacher@gmail.com>
2014-10-01 13:42:30 +0200

    model added

---
+++ models/content_domain_area.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace Concrete\Package\DomainArea\Models;
+
+use Loader,
+    Page,
+    Area;
+
+class ContentDomainArea
+{
+
+    private $areas = [];
+    private $arHandle;
+
+    /**
+     * Creates areas depending on the current domain and the state of the page.
+     * If we're in the edit mode, areas for all domains are shown, no matter what.
+     * 
+     * In case we're not in the edit mode, only those areas are shown which match
+     * the current domain.
+     * 
+     * @param string $arHandle
+     */
+    public function __construct($arHandle)
+    {
+        $this->arHandle = $arHandle;
+        $c = Page::getCurrentPage();
+
+        // get domains
+        // @TODO move this into a separate model
+        $db = Loader::db();
+
+        // show all areas while we're in edit mode
+        if ($c->isEditMode()) {
+            $domains = $db->getAll('SELECT domain FROM DomainAreaDomains ORDER BY domain');
+
+            foreach ($domains as $domain) {
+                $this->areas[] = new Area($this->arHandle . ' (' . $domain['domain'] . ')');
+            }
+        } else {
+            $httpHost = $_SERVER['HTTP_HOST'];
+            if (preg_match("/[^\.\/]+\.[^\.\/]+$/", $httpHost, $matches)) {
+                $currentDomain = $matches[0];
+                $domains = $db->getAll('SELECT domain FROM DomainAreaDomains WHERE domain = ?', array($currentDomain));
+
+                foreach ($domains as $domain) {
+                    $this->areas[] = new Area($this->arHandle . ' (' . $domain['domain'] . ')');
+                }
+            }
+        }
+    }
+
+    /**
+     * Returns the list of all areas, for those who want to work with areas in
+     * their code.
+     * 
+     * @return array
+     */
+    public function getAreas()
+    {
+        return $this->areas;
+    }
+
+    /**
+     * Display areas
+     * 
+     * @param Page $c
+     * @param array $alternateBlockArray
+     */
+    public function display($c, $alternateBlockArray = null)
+    {
+        if (is_array($this->areas)) {
+            foreach ($this->areas as $area) {
+                $area->display($c, $alternateBlockArray);
+            }
+        }
+    }
+
+    /**
+     * Forward certain calls to each area
+     * 
+     * @param string $name
+     * @param array $arguments
+     */
+    public function __call($name, $arguments)
+    {
+        $supportedMethods = [
+            'enableGridContainer',
+            'disableControls',
+            'setBlockLimit',
+            'setAreaGridMaximumColumns',
+            'setBlockWrapperStart',
+            'setBlockWrapperEnd',
+            'setCustomTemplate',
+            'forceControlsToDisplay'
+        ];
+
+        if (in_array($name, $supportedMethods)) {
+            if (is_array($this->areas)) {
+                foreach ($this->areas as $area) {
+                    call_user_func_array(array($area, $name), $arguments);
+                }
+                return;
+            }
+        }
+
+        throw new \Exception(sprintf('%s does not support the method %s', get_class(), $name));
+    }
+
+}
